9b30737476c9b4a05cdcde185f3c073a51df65ca
divya <gopalakrishnandivya6@gmail.com>
2019-03-11 17:14:00 -0600

    Updates

---
+++ precache-manifest.692e5b7a3209861cd8276b0eeaf2c8ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "735e2617e305d3b265b7",
+    "url": "/Clicky-Game/static/js/main.9614b483.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/Clicky-Game/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "735e2617e305d3b265b7",
+    "url": "/Clicky-Game/static/css/main.73cad9ec.chunk.css"
+  },
+  {
+    "revision": "7a08882e095a3ca746df56ff31d6f531",
+    "url": "/Clicky-Game/index.html"
+  }
+];
